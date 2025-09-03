ceaaafb62af448a6b5a56a398463ca21dc8d68ac
priyesshrai <priyeshrai369@gmail.com>
2025-09-03 11:58:42 +0530

    Add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,139 @@
+'use client'
+import { useLenisControl } from '@/utils/SmoothScroll';
+import { Menu, Phone, X } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+interface MenuItem {
+    key: string;
+    name: string;
+    path: string;
+}
+
+export default function NavBar() {
+    const currentPath = usePathname();
+    const menuItems: MenuItem[] = [
+        {
+            key: 'home',
+            name: 'Home',
+            path: '/'
+        },
+        {
+            key: 'about',
+            name: 'About',
+            path: '/about'
+        },
+        {
+            key: 'services',
+            name: 'Services',
+            path: '#'
+        },
+        {
+            key: 'appointment',
+            name: 'Appointment',
+            path: '/#'
+        },
+        {
+            key: 'contact',
+            name: 'Contact',
+            path: '#'
+        },
+        {
+            key: 'blog',
+            name: 'Blogs',
+            path: '#'
+        }
+    ]
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { stopScroll, startScroll } = useLenisControl();
+
+    useEffect(() => {
+        if (isMenuOpen) {
+            stopScroll();
+        } else {
+            startScroll();
+        }
+        return () => startScroll();
+    }, [isMenuOpen, stopScroll, startScroll]);
+
+    return (
+        <header className='relative w-full'>
+            <nav className='relative w-full max-w-7xl mx-auto px-5 py-3 flex items-center justify-between'>
+                <div className='relative shrink-0 w-max'>
+                    <Link href={'/'}>
+                        <Image src={'/images/logo/logo.svg'} width={300} height={200} alt='Dr. Satyanarayana' className='' />
+                    </Link>
+                </div>
+
+                <div className='lg:flex hidden relative w-max items-center gap-4'>
+                    {
+                        menuItems.map(items => (
+                            <Link
+                                href={items.path}
+                                key={items.key}
+                                className={`relative text-base font-medium text-dark-navy transition-all duration-300 ease-linear hover:text-hover-navy after:absolute after:bottom-0 after:h-[2px] after:bg-dark-navy ${currentPath === items.path ? ' after:left-0 after:w-full' : 'after:w-0 after:right-0 after:left-auto'}`}>
+                                {items.name}
+                            </Link>
+                        ))
+                    }
+                </div>
+
+                <div className='lg:flex flex-col items-end hidden relative w-max'>
+                    <div className='relative flex items-center text-dark-navy gap-2'>
+                        <Phone size={16} className='mt-[3px]' />
+                        <Link href={'tel:(555)123-4567'}
+                            className='flex flex-col text-lg text-dark-navy font-semibold leading-[1.2]'>
+                            (+91) 123-4567890
+                        </Link>
+                    </div>
+                    <span className='text-[12px] text-zinc-400 font-medium'>24/7 Emergency Line</span>
+                </div>
+
+                <button onClick={() => setIsMenuOpen((prev) => !prev)} className='lg:hidden w-12 h-12 flex items-center justify-center cursor-pointer bg-primary rounded-full '>
+                    <Menu className='text-dark-navy w-7 h-7' />
+                </button>
+            </nav>
+
+            <div className={`lg:hidden fixed inset-0 left-0 transition-transform duration-200 ease-linear bg-dark-navy/40 z-20 backdrop-blur-[4px] ${isMenuOpen ? 'translate-x-0' : ' -translate-x-full'} `} />
+
+            <div className={`lg:hidden fixed z-40 inset-y-0 bg-white max-w-[450px] w-full transition-transform duration-300 ease-linear p-5 overflow-hidden ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className='w-full h-full relative'>
+                    <div className='relative w-full flex justify-between'>
+                        <Link href={'/'}>
+                            <Image src={'/images/logo/logo.svg'} width={300} height={200} alt='Dr. Satyanarayan' />
+                        </Link>
+                        <button className='w-10 h-10 bg-dark-navy rounded-full flex items-center justify-center border-none cursor-pointer' onClick={() => { setIsMenuOpen(false) }}>
+                            <X color='white' />
+                        </button>
+                    </div>
+
+                    <div className='w-full relative mt-10 flex flex-col gap-2'>
+                        {
+                            menuItems.map(items => (
+                                <Link href={items.path} key={items.key} className={`w-max relative text-xl transition-all font-medium duration-300 ease-linear px-4 py-1.5 rounded-full 
+                                    ${currentPath === items.path ? 'bg-dark-navy text-white' : 'text-dark-navy'} `}
+                                    onClick={() => { setIsMenuOpen(false) }}
+                                >
+                                    {items.name}
+                                </Link>
+                            ))
+                        }
+                    </div>
+
+                    <div className='w-max absolute bottom-0 flex flex-col items-end' onClick={() => { setIsMenuOpen(false) }}>
+                        <div className='relative flex items-center text-dark-navy gap-2'>
+                            <Phone size={16} className='mt-[3px]' />
+                            <Link href={'tel:(555)123-4567'}
+                                className='flex flex-col text-lg text-dark-navy font-semibold leading-[1.2]'>
+                                (+91) 123-4567890
+                            </Link>
+                        </div>
+                        <span className='text-[12px] text-zinc-400 font-medium'>24/7 Emergency Line</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/utils/Section.tsx
+++ src/utils/Section.tsx
@@ -2,53 +2,56 @@ import React, { ButtonHTMLAttributes, CSSProperties } from 'react'
 
 interface Props {
     children: React.ReactNode;
-    classname?: string;
+    className?: string;
     style?: CSSProperties;
 }
 interface BtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
-    classname?: string;
+    className?: string;
     style?: CSSProperties;
 }
 
-export function Section({ children, classname, style }: Props) {
+export function Section({ children, className, style }: Props) {
     return (
-        <section className={classname} style={style}>
+        <section className={className} style={style}>
             {children}
         </section>
     )
 }
-export function Wrapper({ children, classname, style }: Props) {
+export function Wrapper({ children, className, style }: Props) {
     return (
-        <div className={`${classname ?? ''} w-full max-w-7xl relative mx-auto px-6`} style={style}>
+        <div className={`${className ?? ''} w-full max-w-7xl relative mx-auto px-6`} style={style}>
             {children}
         </div>
     )
 }
-export function Heading({ children, classname, style }: Props) {
+export function Heading({ children, className, style }: Props) {
     return (
-        <h2 className={`${classname ?? ''} lg:text-4xl md:text-[28px] text-2xl leading-[1.2] font-bold text-primary-dark`} style={style}>
+        <h2 className={`${className ?? ''} lg:text-4xl md:text-[28px] text-2xl leading-[1.2] font-bold text-primary-dark`} style={style}>
             {children}
         </h2>
     )
 }
-export function Subheading({ children, classname, style }: Props) {
+export function Subheading({ children, className, style }: Props) {
     return (
-        <p className={`${classname ?? ''} lg:text-base text-sm text-zinc-600 mt-1 text-center font-medium`} style={style}>
+        <p className={`${className ?? ''} lg:text-base text-sm text-zinc-600 mt-1 text-center font-medium`} style={style}>
             {children}
         </p>
     )
 }
-export function ButtonPrimary({ children, classname, style, ...rest }: BtnProps) {
+export function ButtonPrimary({ children, className, style, ...rest }: BtnProps) {
     return (
-        <button className={`${classname ?? ''} text-white bg-primary md:text-base text-sm font-semibold px-5 h-11 border-none outline-none cursor-pointer transition-all duration-200 ease-linear hover:bg-[#849d13] rounded-full`} {...rest} style={style}>
+        <button className={`${className ?? ''} text-white bg-dark-navy md:text-base text-sm font-semibold px-5 h-14 border-none outline-none cursor-pointer transition-all duration-300 ease-linear hover:bg-hover-navy rounded-full`}
+            {...rest}
+            style={style}>
             {children}
         </button>
     )
 }
-export function ButtonSecondry({ children, classname, style, ...rest }: BtnProps) {
+export function ButtonSecondry({ children, className, style, ...rest }: BtnProps) {
     return (
-        <button className={`${classname ?? ''} text-white bg-secondry md:text-base text-sm font-semibold px-5 h-11 border-none outline-none cursor-pointer transition-all duration-200 ease-linear hover:bg-[#068AC3] rounded-full`} {...rest}
+        <button className={`${className ?? ''} text-white bg-light-blue md:text-base text-sm font-semibold px-5 h-14 border-none outline-none cursor-pointer transition-all duration-200 ease-linear hover:bg-hover-blue rounded-full`}
+            {...rest}
             style={style}>
             {children}
         </button>
