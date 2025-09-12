f1ac2b2a8f94852dceb519d778244167efc828be
priyesshrai <priyeshrai369@gmail.com>
2025-09-12 15:44:08 +0530

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
+        <link rel="shortcut icon" href="/images/logo/fav-icon.svg" type="image/x-icon" />
       </head>
       <body className="overflow-x-hidden">
         <SmoothScrollProvider>
