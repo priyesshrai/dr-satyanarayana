813a8a56b9efeeeb4907da8eb932ed3ce962c43a
priyesshrai <priyeshrai369@gmail.com>
2025-11-03 16:33:41 +0530

    update video captions

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,7 +6,7 @@ import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { useLenisControl } from '@/utils/SmoothScroll';
 
 export default function Video() {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
     const { stopScroll, startScroll } = useLenisControl();
     useEffect(() => {
         if (open) {
@@ -23,25 +23,30 @@ export default function Video() {
                 <div className='relative w-full flex flex-col lg:gap-14 md:gap-10 gap-8'>
                     <div className='flex-1 text-center'>
                         <Heading>
-                            My Procedural Videos
+                            Procedural Highlights
                         </Heading>
                         <Subheading className='mx-auto max-w-md'>
-                            Demonstrating essential healthcare techniques through easy-to-follow procedural videos.
+                            Insight into advanced treatments.
                         </Subheading>
                     </div>
                     <div className='flex-1 relative grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5'>
-                        <div className="relative w-full h-[400px] flex items-center justify-center rounded-2xl bg-[url(/images/video/img-01.jpg)] bg-no-repeat bg-cover bg-center after:inset-0 after:bg-zinc-800/20 after:absolute overflow-hidden " onClick={() => setOpen(true)}>
-                            <div className="relative">
-                                <div className="ripple w-20 h-20 rounded-full  flex items-center justify-center relative">
-                                    <div className="absolute inset-0 rounded-full animate-ripple bg-dark-navy/40"></div>
-                                    <div className="absolute inset-0 rounded-full animate-ripple delay-300 bg-dark-navy/40"></div>
-                                    <div className="absolute inset-0 rounded-full animate-ripple delay-700 bg-dark-navy/40"></div>
+                        <div className='relative w-full'>
+                            <div className="relative w-full h-[400px] flex items-center justify-center rounded-2xl bg-[url(/images/video/img-01.jpg)] bg-no-repeat bg-cover bg-center after:inset-0 after:bg-zinc-800/20 after:absolute overflow-hidden " onClick={() => setOpen(true)}>
+                                <div className="relative">
+                                    <div className="ripple w-20 h-20 rounded-full  flex items-center justify-center relative">
+                                        <div className="absolute inset-0 rounded-full animate-ripple bg-dark-navy/40"></div>
+                                        <div className="absolute inset-0 rounded-full animate-ripple delay-300 bg-dark-navy/40"></div>
+                                        <div className="absolute inset-0 rounded-full animate-ripple delay-700 bg-dark-navy/40"></div>
 
-                                    <div className="relative z-10 flex items-center justify-center w-20 h-20 bg-gray-100 rounded-full cursor-pointer text-dark-navy shadow-lg hover:scale-105 transition-transform duration-300">
-                                        <Play size={40} strokeWidth={1.5} />
+                                        <div className="relative z-10 flex items-center justify-center w-20 h-20 bg-gray-100 rounded-full cursor-pointer text-dark-navy shadow-lg hover:scale-105 transition-transform duration-300">
+                                            <Play size={40} strokeWidth={1.5} />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <span className='block mt-2 text-xl font-medium text-dark-navy'>
+                                Saving an Access, Restoring Dialysis
+                            </span>
                         </div>
 
                     </div>
@@ -73,7 +78,7 @@ function Drawer({ open, setOpen }: Props) {
                     />
 
                     <motion.div
-                        className="fixed bottom-0 left-0 right-0 bg-dark-navy rounded-t-3xl shadow-2xl z-50 overflow-y-auto h-[550px]"
+                        className="fixed bottom-0 left-0 right-0 bg-dark-navy rounded-t-3xl shadow-2xl z-50 overflow-y-auto h-[600px]"
                         initial={{ y: "100%" }}
                         animate={{ y: "8%" }}
                         exit={{ y: "100%" }}
