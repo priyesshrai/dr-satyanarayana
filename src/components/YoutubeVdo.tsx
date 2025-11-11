a42821022eaa80a877a0bf6b5cc983f3aa680f7b
priyesshrai <priyeshrai369@gmail.com>
2025-11-11 15:50:25 +0530

    Add youtube video

---
+++ src/components/YoutubeVdo.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { Section, Wrapper } from '@/utils/Section'
+import React, { useRef, useState } from 'react'
+import {
+    Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay";
+
+export default function YoutubeVdo() {
+    const ytLinks: string[] = [
+        "https://www.youtube.com/embed/qHCl4Jl8--M?si=9aDB8TBX5_-DfGz6",
+        "https://www.youtube.com/embed/h6r_UO5GUL0?si=InuNH1lQMi6Ke5si",
+        "https://www.youtube.com/embed/IAAWhsaMXoI?si=ngIuy8o_n2ShEHB0",
+        "https://www.youtube.com/embed/RvIC4tfn3Vc?si=5kr32w-WfvjFjXBy",
+        "https://www.youtube.com/embed/DbYZGAMZpbc?si=tbduCirPfO27uU8Q",
+        "https://www.youtube.com/embed/an0UCsNB55g?si=Kri2nCWaP-fp-FN4"
+    ]
+    const [api, setApi] = useState<CarouselApi>()
+    const plugin = useRef(Autoplay({ delay: 4000, stopOnInteraction: false, stopOnMouseEnter: true }))
+    return (
+        <Section className='!px-0'>
+            <Wrapper className='!max-w-full !p-0'>
+                <div className='w-full h-full overflow-hidden'>
+                    <Carousel className="w-full h-full relative"
+                        plugins={[plugin.current]}
+                        opts={{
+                            loop: true,
+                            dragFree: true
+                        }}
+                        onMouseEnter={plugin.current.stop}
+                        onMouseLeave={plugin.current.reset}
+                        setApi={setApi}
+                    >
+                        <CarouselContent className='items-center gap-3'>
+                            {
+                                ytLinks.map((links, idx) => (
+                                    <CarouselItem
+                                        key={idx}
+                                        className="max-w-[500px] w-full shrink-0 cursor-pointer bg-white rounded-2xl  overflow-hidden"
+                                    >
+                                        <div className="relative w-full pt-[56.25%]">
+                                            <iframe
+                                                src={links}
+                                                title={`YouTube video ${idx}`}
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                                referrerPolicy="strict-origin-when-cross-origin"
+                                                allowFullScreen
+                                                className="absolute top-0 left-0 w-full h-full rounded-2xl border-none"
+                                            ></iframe>
+                                        </div>
+                                    </CarouselItem>
+                                ))
+                            }
+                        </CarouselContent>
+                        <CarouselPrevious
+                            onClick={() => {
+                                api?.scrollPrev();
+                                plugin.current?.reset();
+                            }}
+                            className='w-11 h-11 bg-dark-navy text-white left-4 text-3xl cursor-pointer hover:bg-primary-hover hover:text-white'
+                        />
+                        <CarouselNext
+                            onClick={() => {
+                                api?.scrollNext();
+                                plugin.current?.reset();
+                            }}
+                            className='w-11 h-11 bg-dark-navy text-white text-3xl right-4 cursor-pointer hover:bg-primary-hover hover:text-white'
+                        />
+                    </Carousel>
+                </div>
+            </Wrapper>
+        </Section>
+    )
+}
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/qHCl4Jl8--M?si=9aDB8TBX5_-DfGz6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */ }
